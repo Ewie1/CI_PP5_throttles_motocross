ae0c361c87e6a075026556689bbf6df1e47743eb
Ewie1 <>
2023-05-21 10:16:22 +0000

    Create category and product admin and load data

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
 
-# Register your models here.
+# Internal
+from .models import Category, Product
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class categoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class productAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'brand',
+        'price',
+        'rating',
+        'category',
+        'image',
+        'image_url',
+    ) 
+
+    ordering = ('category', 'brand')
+
+
+admin.site.register(Product)
+admin.site.register(Category)
