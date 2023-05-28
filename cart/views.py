fb98cc171d39f67f9f56cfd7e39b2c5f078f87e7
Ewie1 <>
2023-05-28 14:12:50 +0000

    Create shopping cart view and url to render cart page

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_cart(request):
+    """
+    A view to display the basket page
+    """
+    return render(request, 'cart/cart.html')
