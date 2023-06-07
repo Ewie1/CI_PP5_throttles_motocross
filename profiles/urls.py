7c17913cf60ae87ee758b9c7bfb296a52d1bc7b1
Ewie1 <>
2023-06-07 12:56:15 +0000

    Add view and profile template to render profile page

---
+++ profiles/urls.py
@@ -0,0 +1,14 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
+from django.urls import path
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+urlpatterns = [
+    path('', views.profile, name='profile'),
+    path('order_history/<order_number>',
+         views.order_history, name='order_history'),
+]
