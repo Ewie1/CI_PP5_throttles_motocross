db13a4a142817df4f11513d63609859681c2a176
Ewie1 <>
2023-06-08 17:55:45 +0000

    Add product form to handle product editing

---
+++ products/forms.py
@@ -0,0 +1,27 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
+from django import forms
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import Product, Category
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories_list = Category.objects.all()
+        friendly_names = [
+            (c.id, c.get_friendly_name()) for c in categories_list
+        ]
+        self.fields['category'].label = 'Category'
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-secondary rounded'
