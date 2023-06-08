b4002aaacdad8ad4c7333a5d1638637f87640d08
Ewie1 <>
2023-06-08 18:44:55 +0000

    Add view and url to delete products

--- products/views.py
+++ products/views.py
@@ -90,3 +90,18 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete product from the shop
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Only Admins owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product has been deleted.')
+
+    return redirect(reverse('products'))
