1c53195bb056c9f3bba4b509310cd864f6975084
Ewie1 <>
2023-06-15 16:27:55 +0000

    Deployment attempt 3

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-57y!*e-1f8ok=rf=h7&2ypi2_tccqo_rugzfn6#$rije14=^rl'
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-57y!*e-1f8ok=rf=h7&2ypi2_tccqo_rugzfn6#$rije14=^rl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['ci-pp5-throttles-motocross.heroku.com', 'localhost']
 
