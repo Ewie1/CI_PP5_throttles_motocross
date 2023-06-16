54c5ff5d1d6ff4538158639d14149e6d03477892
Ewie1 <>
2023-06-16 17:19:24 +0000

    Delete redundant code from settings file

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-57y!*e-1f8ok=rf=h7&2ypi2_tccqo_rugzfn6#$rije14=^rl'
-
 SECRET_KEY = os.environ.get('SERCRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
