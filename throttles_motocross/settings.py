e233090b2ef4fddcff788f0f3ae6fd8c8751759e
Ewie1 <>
2023-06-16 17:43:55 +0000

    Correct typo error in setting.py

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -198,7 +198,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    #Bucket Config
+    # Bucket Config 
     AWS_STORAGE_BUCKET_NAME = 'throttlesmotocross'
     AWS_S3_REGION_NAME = 'EU (Stockholm) eu-north-1'
     AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
