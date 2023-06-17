881176c8bcec4687e1ecc53fc1635bf6c49985e5
Ewie1 <>
2023-06-17 07:38:14 +0000

    Add secret key for deployment

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SERCRET_KEY')
+SECRET_KEY = os.environ.get('SERCRET_KEY', 'django-insecure-57y!*e-1f8ok=rf=h7&2ypi2_tccqo_rugzfn6#$rije14=^rl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
