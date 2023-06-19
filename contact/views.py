1eeeede59ab709278729bde90ab045153cfa9d9f
Ewie1 <>
2023-06-19 22:11:20 +0000

    Add view to render contact form and inpust user information

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
+from .forms import ContactForm
+from products.models import Product, Category
 # This will get the user information
 
 
@@ -19,4 +21,22 @@ class ContactFeedback(View):
     template_name = 'contact/contact.html'
     success_message = 'Message has been sent.'
 
-    
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieves users email and inputs into email input
+        """
+        categories_list = Category.objects.all()
+
+        if request.user.is_authenticated:
+            email = request.user.email
+            contact_form = ContactForm(initial={'email': email})
+        else:
+            contact_form = ContactForm()
+
+        context = {
+            'categories_list': categories_list,
+            'contact_form': contact_form
+        }
+
+        return render(request, 'contact/contact.html',
+                      context)
