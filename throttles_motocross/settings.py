e1fe9af092cbb72c83232adcdef3ff2a93e3a68e
Ewie1 <>
2023-10-01 13:45:20 +0000

    Add correct format to settings.py vars

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = [os.environ.get('SERCRET_KEY')]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-development = os.environ.get('DEVELOPMENT', False)
+development = [os.environ.get('DEVELOPMENT', False)]
 
 DEBUG = development
 
@@ -220,8 +220,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'throttlesmotocross'
     AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY = [os.environ.get('AWS_ACCESS_KEY_ID')]
+    AWS_SECRET_ACCESS_KEY = [os.environ.get('AWS_SECRET_ACCESS_KEY')]
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
@@ -247,12 +247,12 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 FREE_DELIVERY_THRESHOLD = 49
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'sek'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY',
-                              '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY',
-                              '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET',
-                             '')
+STRIPE_PUBLIC_KEY = [os.getenv('STRIPE_PUBLIC_KEY',
+                              '')]
+STRIPE_SECRET_KEY = [os.getenv('STRIPE_SECRET_KEY',
+                              '')]
+STRIPE_WH_SECRET = [os.getenv('STRIPE_WH_SECRET',
+                             '')]
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -262,6 +262,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = [os.environ.get('EMAIL_HOST_USER')]
+    EMAIL_HOST_PASSWORD = [os.environ.get('EMAIL_HOST_PASS')]
+    DEFAULT_FROM_EMAIL = [os.environ.get('EMAIL_HOST_USER')]
