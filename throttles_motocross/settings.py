c03d68113c069ae2e61dc0e460ce670612a32bef
Ewie1 <>
2023-10-01 13:54:31 +0000

    Remove redundant code from settings file

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -220,8 +220,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'throttlesmotocross'
     AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY = [os.environ.get('AWS_ACCESS_KEY_ID')]
-    AWS_SECRET_ACCESS_KEY = [os.environ.get('AWS_SECRET_ACCESS_KEY')]
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
@@ -262,6 +262,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = [os.environ.get('EMAIL_HOST_USER')]
-    EMAIL_HOST_PASSWORD = [os.environ.get('EMAIL_HOST_PASS')]
-    DEFAULT_FROM_EMAIL = [os.environ.get('EMAIL_HOST_USER')]
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
