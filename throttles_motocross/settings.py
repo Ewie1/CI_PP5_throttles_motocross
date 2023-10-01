b015f854633acb5b3bc62a017425a0df84994a54
Ewie1 <>
2023-10-01 13:57:37 +0000

    Removed redundant code

--- throttles_motocross/settings.py
+++ throttles_motocross/settings.py
@@ -247,12 +247,12 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 FREE_DELIVERY_THRESHOLD = 49
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'sek'
-STRIPE_PUBLIC_KEY = [os.getenv('STRIPE_PUBLIC_KEY',
-                              '')]
-STRIPE_SECRET_KEY = [os.getenv('STRIPE_SECRET_KEY',
-                              '')]
-STRIPE_WH_SECRET = [os.getenv('STRIPE_WH_SECRET',
-                             '')]
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY',
+                              '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY',
+                              '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET',
+                             '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
